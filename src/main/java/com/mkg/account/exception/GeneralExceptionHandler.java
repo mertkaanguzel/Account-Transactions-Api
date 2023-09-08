d45f9dda81fddf3c331397e860b523880fef5c32
rtardo <84642031+rtardo@users.noreply.github.com>
2023-09-08 15:37:47 +0300

    Add Dockerfile, docker-compose

--- src/main/java/com/mkg/account/exception/GeneralExceptionHandler.java
+++ src/main/java/com/mkg/account/exception/GeneralExceptionHandler.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
     @NotNull
